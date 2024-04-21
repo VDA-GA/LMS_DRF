78edea8e637cc361e26728d6674186793fd1ca4b
Dmitry Vasilenko <vasilenkoda91@gmail.com>
2024-04-21 13:49:07 +0300

    HW-2 Task-1

--- lms/serializers.py
+++ lms/serializers.py
@@ -4,6 +4,11 @@ from lms.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lesson_count = serializers.SerializerMethodField()
+
+    def get_lesson_count(self, course):
+        return course.lesson_set.count()
+
     class Meta:
         model = Course
         fields = "__all__"
