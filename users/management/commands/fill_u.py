36c0983ffb65d382b8c3f0ceb9e665c7d3047059
Dmitry Vasilenko <vasilenkoda91@gmail.com>
2024-04-21 15:15:50 +0300

    HW-2 Task-2

---
+++ users/management/commands/fill_u.py
@@ -0,0 +1,15 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        list_users = [
+            {'email': 'test1@mail.ru'},
+            {'email': 'test2@mail.ru'},
+            {'email': 'test3@mail.ru'},
+        ]
+
+        for user_item in list_users:
+            User.objects.create(**user_item)
--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from lms.models import Course, Lesson
+
 NULLABLE = {"blank": True, "null": True}
 
 
@@ -13,3 +15,24 @@ class User(AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+
+class Payment(models.Model):
+    class Method(models.TextChoices):
+        CARD = 'перевод на карту'
+        CASH = 'наличными'
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='пользователь')
+    date = models.DateTimeField(auto_now_add=True, verbose_name='дата оплаты')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, **NULLABLE, verbose_name='оплаченный курс')
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, **NULLABLE, verbose_name='оплаченный урок')
+    method = models.CharField(max_length=20, choices=Method.choices, verbose_name='способ оплаты')
+
+    def __str__(self):
+        return f'{self.user} - {self.course if self.course else self.lesson}'
+
+    class Meta:
+        verbose_name = 'платеж'
+        verbose_name_plural = 'платежи'
+        ordering = ('-date',)
+
